2827c17c55dc5257a5ec6d578d7423e32f8c4cd6
Ingvi Rafn Hafþórsson <ingvih@gmail.com>
2018-09-09 09:19:27 +0000

    Added react-loadable and trying dynamic imports.

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,27 @@
 
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
+import Loadable from 'react-loadable';
+
 import Home from './Home'
-import News from './News'
-import Contact from './Contact'
+// import News from './News'
+// import Contact from './Contact'
 import Login from './containers/Login'
 import NotFound from './containers/NotFound'
+
+const LoadableNews = Loadable({
+  loader: () => import('./News'),
+  loading() {
+    return <div>Loading...</div>
+  }
+});
+
+const LoadableContact = Loadable({
+  loader: () => import('./Contact'),
+  loading() {
+    return <div>Loading...</div>
+  }
+});
 
 class Main extends Component {
   render() {
@@ -13,10 +29,10 @@ class Main extends Component {
       <div className="content">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/news" component={News} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/news" component={LoadableNews} />
+          <Route path="/contact" component={LoadableContact} />
           <Route path="/login" exact component={Login} />
-          <Route component={NotFound} />
+          <Route component={NotFound} status={404} />
         </Switch>
       </div>
     );
